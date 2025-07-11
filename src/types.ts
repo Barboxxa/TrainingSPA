2567bc6806fce6604136bf1b82a56618e56a26c8
João Ricardo Barbosa <79670312+Barboxxa@users.noreply.github.com>
2025-07-11 11:06:33 -0300

    first commit
    commit project

---
+++ src/types.ts
@@ -0,0 +1,34 @@
+// src/types.ts
+
+export interface Exercise {
+  name: string;
+  details?: string; // Para supersets ou observações
+}
+
+export interface DailyWorkout {
+  day: string;
+  group: string;
+  exercises: Exercise[];
+  details?: string;
+}
+
+export interface MonthlyPlan {
+  month: number;
+  title: string;
+  focus: string;
+  sets: string;
+  reps: string;
+  rest: string;
+  schedule: DailyWorkout[];
+}
+
+export interface RunningPhase {
+  id: number;
+  weeks: string;
+  title: string;
+  details: {
+    aquecimento: string;
+    treino: string;
+    desaquecimento: string;
+  };
+}
